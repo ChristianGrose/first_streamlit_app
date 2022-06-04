916e50033d3be3801fd1a05537cdeb8b9b995799
Christian Grose <106867465+ChristianGrose@users.noreply.github.com>
2022-06-04 12:58:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('my parents new healthy diner')
 
-Streamlit.header('Breakfast Menu')
-streamlit.text('Omeg3 and blueberry oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
 streamlit.text('Kale, spinach & rocket smoothie')
 streamlit.text('hard-boild and free-range egg')
